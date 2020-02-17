47bfcca99a4e74f4adbf8d49500241b94dd4842c
SparrOSDeveloperTeam <SparrOSDeveloperTeam@users.noreply.github.com>
2020-02-16 22:36:30 -0600

    Create linux cpp

---
+++ dedserv.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstdlib>
+
+int main() {
+    std::system("./hostname");
+    std::cout << '\n' << "Dedicated server for Gossamer. Version 0.9.22.447.\n";
+    return 0;
+}

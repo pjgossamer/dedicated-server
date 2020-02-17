826fcd0d540a6a7d71b7c3b9d54901d674856e19
SparrOSDeveloperTeam <SparrOSDeveloperTeam@users.noreply.github.com>
2020-02-16 22:36:53 -0600

    Forgot important lines

--- win32/dedserv/dedserv.cpp
+++ win32/dedserv/dedserv.cpp
@@ -3,5 +3,6 @@
 
 int main () {
     Process::Start("hostname.exe");
-    std::cout << '\n' << "Dedicated server for Gossamer. Version 0.9.22.447.\n"
+    std::cout << '\n' << "Dedicated server for Gossamer. Version 0.9.22.447.\n";
+    return 0;
 }

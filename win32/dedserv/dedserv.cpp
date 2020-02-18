35ac0d4d08e30e001c7ee180b47b1962fc58e3eb
SparrOSDeveloperTeam <SparrOSDeveloperTeam@users.noreply.github.com>
2020-02-17 21:04:08 -0600

    Update dedserv.cpp

--- win32/dedserv/dedserv.cpp
+++ win32/dedserv/dedserv.cpp
@@ -7,7 +7,7 @@ using namespace System::Diagnostics;
 int main(array<System::String ^> ^args)
 {
     Process::Start("hostname.exe");
-    std::cout << '\n' << "Dedicated server for Gossamer. Version 0.9.22.447.\n";
+    std::cout << '\n' << "Dedicated server for Gossamer. Version 0.9.22.447 (Beta).\n";
     Process::Start("data\\binaries\\win64\\pjgossamer-dedserv.exe", "-log");
     return 0;
 }
